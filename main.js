40d2a221bf0d9ef76ee1e1e49e64e5c42b3699f8
Oleksandr Frentsel <Oleksandr.Frentsel@sophos.com>
2019-04-16 19:56:40 +0300

    implemented ajax way

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var content = document.getElementById('content');
 
 window.onhashchange = function() {
-content.innerHTML = document.getElementById(location.hash.slice(1))
-  .innerHTML;
+  var xhr = new XMLHttpRequest();
+  var pageName = location.hash.slice(1);
+  xhr.open('get', `pages/${pageName}.html`);
+  xhr.send();
+  xhr.onload = function(){
+    content.innerHTML = this.responseText;
+  }
 };
 
 location.hash = '#home';
